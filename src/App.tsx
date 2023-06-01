241d10b6f535f2eeb8a14694620cd4f39ffdc770
luvlaceeeee <nikitan2002@gmail.com>
2023-06-01 18:24:29 +0300

    feat: add commitlint to husky

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import { useState } from 'react';
 export function App() {
   const [count, setCount] = useState<number>(0);
   return (
-    <div className="flex flex-col items-center justify-between space-y-3">
-      <h1 className="text-2xl font-bold">Boilerplate setup</h1>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="mb-3 text-4xl font-bold">React app Boilerplate</h1>
+      <h2 className="mb-10 text-lg font-medium">
+        Include: TypeScript, React, Vite, Tailwind, ESLint, Prettier, Husky,
+        AirBnb
+      </h2>
+
       <button
-        className="rounded-xl bg-red-300 px-5 py-3"
+        className="rounded-xl bg-red-300 px-5 py-3 text-xl"
         onClick={() => setCount((already) => already + 1)}
       >
         count is {count}
